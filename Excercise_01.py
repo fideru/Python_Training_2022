15ee48057b07416fd98fb92c4cc6eab13e251b85
baezl <baezlopezfidel24@gmail.com>
2022-06-15 22:01:24 +0900

    Merge remote-tracking branch 'origin/main'

--- Excercise_01.py
+++ Excercise_01.py
@@ -6,3 +6,20 @@ for number in range (1, 18):
         counter += 1
         print(number)
 print(f"We have {counter} even numbers")
+
+
+# Fizz Buzz
+
+def fizz_buzz(digit_1):
+
+    if (digit_1 % 5 == 0) and (digit_1 % 3 == 0):
+        return "FizzBuzz"
+    elif digit_1 % 5 == 0:
+        return "Buzz"
+    elif digit_1 % 3 == 0:
+        return "Fizz"
+    return "Unusable digit"
+
+
+digit_1 = int(input("Enter first digit:"))
+print(fizz_buzz(digit_1))
