0ac95d2273e58e1fcfe4e945c7f4c7abc960b686
Fideru <66935701+fideru@users.noreply.github.com>
2022-06-15 19:44:40 +0900

    Add files via upload

---
+++ Digits.py
@@ -0,0 +1,26 @@
+import math
+
+x = 1
+y = 2.1
+z = 12j
+
+print(5 + 5) # Adittion
+print(5 - 5) # Substraction
+print(5 * 5) # Multiplication
+print(29 / 5) # Division float
+print(29 // 5) # Division int
+print(29 % 5) # Residuo
+print(5 ** 5) # Potencia
+
+x = x + 1
+x += 1
+
+print(math.ceil(2.6))
+
+x = input('x:')
+y = int(x) + 1
+print(y)
+
+# Boolean kinda values
+# Falsy
+# "", 0, none
