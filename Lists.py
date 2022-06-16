ca0b76cc4f3c7b23aab408d346d18e3cea7e99b1
baezl <baezlopezfidel24@gmail.com>
2022-06-16 17:04:15 +0900

    Merge lists

---
+++ Lists.py
@@ -0,0 +1,11 @@
+# lists are put inside brackets[]
+
+letters = ["a", "b", "c", "d", "e","f"]
+matrix = [[2, 11], [13, 2]]
+zeros = [0] * 10
+combined = zeros + matrix
+print(combined)
+numbers = list(range(150))
+chars = list("Hello World")
+print(chars)
+print(len(chars))
---
+++ Scrapper.py
@@ -0,0 +1,13 @@
+# Web Scrapping Library
+from bs4 import BeautifulSoup
+
+# http requests library
+import requests
+
+# email control library
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+# System date/time control
+import datetime
+
