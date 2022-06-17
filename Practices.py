21a4a1abf9845e018bec387a7a8d126b15944da4
baezl <baezlopezfidel24@gmail.com>
2022-06-18 00:05:06 +0900

    More Training
    # Conflicts:
    #	Lists.py

--- Practices.py
+++ Practices.py
@@ -1,24 +1,37 @@
 # 100 Years Old Practice
 import datetime
 
+
 def century_calculator(age):
     year = datetime.date.today().year
     century = (year - age) + 100
     return century
 
+
 print((century_calculator(33)))
+
 
 # Integer ramge sum
 def add_it_up(integer):
-
     try:
-        result_value = sum(range(integer +1))
+        result_value = sum(range(integer + 1))
         return result_value
     except:
         TypeError
         return 0
 
+
 print(add_it_up(5))
 
+# Most Repeated Character on Text
 
-# Practice control Command
+sentence = "This is a common interview question"
+char_dict = {}
+for i in sentence:
+    if i in char_dict:
+        char_dict[i] += 1
+    else:
+        char_dict[i] = 1
+
+final = sorted(char_dict.items(), key=lambda kv: kv[1], reverse=True)
+print("The maximum repeated value is: ", final[0])
