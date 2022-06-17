c14e471fe83ae8e2578e55a7e08bb72133223453
baezl <baezlopezfidel24@gmail.com>
2022-06-17 23:14:29 +0900

    Commit of Data Structures Training

--- Lists.py
+++ Lists.py
@@ -9,9 +9,11 @@ numbers = list(range(150))
 chars = list("Hello World")
 print(chars)
 print(len(chars))
-
 print(letters[0:3])
-
+# list comprehension
+# [expression for item in items]
+letters = {x * 2 for x in range(5)}
+print(letters)
 
 # Sets
 # Sets are collection of values with no duplicates
@@ -38,12 +40,27 @@ print(second)
 point_dictionary = dict(name="John", phone="859-589-8547")
 point_dictionary["gender"] = "Male" # add one more key value to dictionary
 point_dictionary["name"] = "Juan" # change value of key in dictionary
+# Search for a specific key in a dictionary
+if "a" in point_dictionary:
+    print("a")
+#print(point_dictionary.get("a", 0)) # returns 0 as a result in case of not found
+#del point_dictionary["a"] # deletes a key in the dictionary
+# looping dictionaries
+for key in point_dictionary:
+    print(key, point_dictionary[key])
+for x in point_dictionary.items():
+    print(x)
+# Dictionary comprehensions
+values1 = {x: x * 2 for x in range(5)}
+print(values1)
+# Generator Expressions
+# are iterable object generators
 
-
-
-
-
-
+# Unpacking Operator
+numbers = [1, 2, 3]
+print(*numbers) # the * works as an unpacking of the list.
+print([*range(5), *"Hello"])
+# can unpack lists, strings, dictionaries, tuples
 
 
 
